6a1b1d5ae173bba985356ce9ce1d2f6a56c23a92
Oksana Odynska <odynska.oksana@refinitiv.com>
2019-09-03 17:18:41 +0300

    Homework 6 - csv column return

---
+++ homework6/csvReader.py
@@ -0,0 +1,75 @@
+# encoding=utf-8
+import re
+import optparse
+import datetime
+import traceback
+from collections import OrderedDict
+import csv
+import re
+
+
+
+def column_return():
+    parser.add_option("--path_csv", action="store", type="string", dest="path_csv", default=None,
+                      help="Name of csv file to process, required parameter")
+    parser.add_option("--col_name", action="store", type="string", dest="col_name", default=None,
+                      help="Column name of csv file to return, required parameter")
+
+
+    (options, args) = parser.parse_args()
+
+    if not options.path_csv and not options.col_name:  # if no command is specified
+        parser.error('Missing parameters: --path_csv, --col_name')
+    elif not options.path_csv or not options.col_name:
+        parser.error('One of parameters is missing: --path_csv, --col_name')
+
+    fileLocation = options.path_csv
+    columnToReturn = options.col_name
+
+    listWithDicts = []
+    with open(fileLocation, 'r') as csv_file:
+        csv_read = csv.reader(csv_file)
+        headers = next(csv_read)
+        headers_updated = []
+        for item in headers:
+            newstring = re.match(r"(\w+\s\w+)", item)
+            if newstring:
+                replaced = re.sub(r"\s", "_", item)
+                headers_updated.append(replaced)
+            else:
+                headers_updated.append(item)
+
+        for row in csv_read:
+            new = OrderedDict(zip(headers_updated, row))
+            listWithDicts.append(new.copy())
+
+    for item in listWithDicts:
+        print(item[columnToReturn])
+
+
+        # csv_reader = csv.DictReader(csv_file, delimiter=',')
+        # for lines in csv_reader:
+            # print(lines[columnToReturn])
+
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    listWithWords = []
+    wordCountsStorage = {}
+    try:
+        print('\n')
+        print('-----start-----')
+        print('\n')
+        start = datetime.datetime.now()
+        column_return()
+    except Exception as e:
+        print("\nError")
+        print(e)
+        print('\n')
+        traceback.print_exc()
+    finally:
+        duration = str(datetime.datetime.now() - start)  # print the time taken for script execution
+        print('\n')
+        print('command run complete in duration: %s' % duration)
+        print('-----end-----')
